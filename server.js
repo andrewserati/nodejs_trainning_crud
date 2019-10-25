72a697855b38ec5570a5be545b4d4b7fbc386e40
sirlordx <andrewse2015@hotmail.com>
2019-10-25 14:53:49 -0300

    add CREATE and READ

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://andrew_node:admin123@andrewcluster-oklwj.mongodb.net/test?retryWrites=true&w=majority"
-const kennedy = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+const database = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 const app = express();
 
 /*
@@ -12,7 +13,7 @@ client.connect(err => {
 });
 */
 
-kennedy.connect((err, client) => {
+database.connect((err, client) => {
     if (err) return console.log(err);
     db = client.db('andrew-node');
 
@@ -30,14 +31,22 @@ app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
+app.get('/', (req, res) => {
+    var cursor = db.collection('data').find();
+});
+
+app.get('/show', (req, res) => {
+    db.collection('data').find().toArray((err,results) => {
+        if (err) return console.log(err);
+        res.render('show.ejs', {data: results})
+    })
+})
+
 app.post('/show', (req,res) => {
     db.collection('data').insertOne(req.body, (err, result) => {
         if (err) return console.log(err);
 
         console.log('Salvo no banco de dados');
-        res.redirect('/');
-        db.collection('data').find().toArray((err, results) => {
-            console.log(results);
-        })
+        res.redirect('/show');
     })
 });
